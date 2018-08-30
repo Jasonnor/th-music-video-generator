2ba413bd5f1f12837c7224602ed46a0599cf468b
Jasonnor <wujason810@gmail.com>
2018-08-30 22:22:08 +0800

    Remove test lines

--- scripts/image.js
+++ scripts/image.js
@@ -1,20 +1,2 @@
 var body = document.getElementsByTagName('body')[0];
 body.style.backgroundImage = 'url(\'/images/紅美鈴/49613176_p0.jpg\')';
-
-function saveToFirebase() {
-    var dataObject = {
-        game: '東方紅魔郷',
-        song_no: '07',
-        name: '明治十七年の上海アリス',
-        character: '紅美鈴'
-    };
-
-    firebase.database().ref('song').push().set(dataObject)
-        .then(function (snapshot) {
-            console.log('success');
-        }, function (error) {
-            console.log('error, ' + error);
-        });
-}
-
-saveToFirebase();
