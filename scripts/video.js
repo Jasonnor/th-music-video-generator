61e44fc8b1516aac8cecc7f068d7fcaae8eb5bbe
Jasonnor <wujason810@gmail.com>
2018-08-31 14:08:18 +0800

    Add image fadein

--- scripts/video.js
+++ scripts/video.js
@@ -5,7 +5,7 @@ const delay = ms => {
 };
 
 const changeImage = async (charaName, sleep) => {
-    console.log("Change image to character " + charaName);
+    console.log('Change image to character ' + charaName);
     var imageList = []
     firebase.database().ref('images').once('value').then(function (charas) {
         charas.val().forEach(chara => {
@@ -17,12 +17,46 @@ const changeImage = async (charaName, sleep) => {
         });
         if (imageList.length > 0) {
             var imageURL = imageList[Math.floor(Math.random() * imageList.length)];
-            var body = document.getElementsByTagName('body')[0];
-            body.style.backgroundImage = 'url(\'.' + imageURL + '\')';
+            //var body = document.getElementsByTagName('body')[0];
+            //body.style.backgroundImage = 'url(\'.' + imageURL + '\')';
+            fadeInImage('wrapper', 'url(\'.' + imageURL + '\')', 'body');
         }
     });
     if (sleep == true) {
         await delay(5000);
         changeImage(charaName, false);
     }
+}
+
+function setOpacity(object, opacityPct) {
+    object.style.opacity = opacityPct / 100;
+}
+
+function changeOpacity(id, msDuration, msStart, fromO, toO) {
+    var object = document.getElementById(id);
+    var opacity = object.style.opacity * 100;
+    var msNow = (new Date()).getTime();
+    opacity = fromO + (toO - fromO) * (msNow - msStart) / msDuration;
+    if (opacity < 0)
+        setOpacity(object, 0)
+    else if (opacity > 100)
+        setOpacity(object, 100)
+    else {
+        setOpacity(object, opacity);
+        object.timer = window.setTimeout('changeOpacity(\'' + id + '\',' + msDuration + ',' + msStart + ',' + fromO + ',' + toO + ')', 1);
+    }
+}
+
+function fadeInImage(foregroundId, newImage, backgroundId) {
+    var foreground = document.getElementById(foregroundId);
+    var background = document.getElementById(backgroundId);
+    if (background) {
+        background.style.backgroundImage = foreground.style.backgroundImage;
+        background.style.backgroundRepeat = 'no-repeat';
+    }
+    setOpacity(foreground, 0);
+    foreground.style.backgroundImage = newImage;
+    if (foreground.timer) window.clearTimeout(foreground.timer);
+    var startMS = (new Date()).getTime();
+    foreground.timer = window.setTimeout('changeOpacity(\'' + foregroundId + '\',1000,' + startMS + ',0,100)', 10);
 }
