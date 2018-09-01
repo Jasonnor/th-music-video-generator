35ed5a013826f377f07729df8ed33d333b667d94
Jasonnor <wujason810@gmail.com>
2018-09-01 13:27:48 +0800

    Fix song not exist playing bug & Update video restart checker

--- scripts/player.js
+++ scripts/player.js
@@ -58,11 +58,16 @@ Player.prototype = {
    * Play a song in the playlist.
    * @param  {Number} index Index of the song in the playlist (leave empty to play the first or current).
    */
-  play: function (index) {
+  play: function (index, isNewSong) {
     var self = this;
     var sound;
 
     index = typeof index === 'number' ? index : self.index;
+    // Keep track of the index we are currently playing.
+    self.index = index;
+    console.log('Playing index ' + index);
+
+    // Skip song not exist
     var data = self.playlist[index];
     if (data.file == null) {
       self.skip('next');
@@ -122,7 +127,7 @@ Player.prototype = {
     track.innerHTML = data.title;
 
     // Play video
-    if (videoPlayer.stopped) {
+    if (videoPlayer.stopped || isNewSong) {
       changeImage(data.character, true);
     } else if (videoPlayer.paused) {
       videoPlayer.play();
@@ -137,9 +142,6 @@ Player.prototype = {
       playBtn.style.display = 'none';
       pauseBtn.style.display = 'none';
     }
-
-    // Keep track of the index we are currently playing.
-    self.index = index;
   },
 
   /**
@@ -191,114 +193,110 @@ Player.prototype = {
    * Skip to a specific track based on its playlist index.
    * @param  {Number} index Index in the playlist.
    */
-  skipTo: async (index) => {
-      var self = player;
+  skipTo: function (index) {
+    var self = this;
 
-      // Stop the current track.
-      if (self.playlist[self.index].howl) {
-        self.playlist[self.index].howl.stop();
-      }
-
-      // Reset progress.
-      progress.style.width = '0%';
-      if (!videoPlayer.stopped) {
-        videoPlayer.stop();
-      }
-
-      await delay(300);
-      // Play the new track.
-      self.play(index);
-    },
-
-    /**
-     * Set the volume and update the volume slider display.
-     * @param  {Number} val Volume between 0 and 1.
-     */
-    volume: function (val) {
-      var self = this;
-
-      // Update the global volume (affecting all Howls).
-      Howler.volume(val);
-
-      // Update the display on the slider.
-      var barWidth = (val * 90) / 100;
-      barFull.style.width = (barWidth * 100) + '%';
-      sliderBtn.style.left = (window.innerWidth * barWidth + window.innerWidth * 0.05 - 25) + 'px';
-    },
-
-    /**
-     * Seek to a new position in the currently playing track.
-     * @param  {Number} per Percentage through the song to skip.
-     */
-    seek: function (per) {
-      var self = this;
-
-      // Get the Howl we want to manipulate.
-      var sound = self.playlist[self.index].howl;
-
-      // Convert the percent into a seek position.
-      if (sound.playing()) {
-        sound.seek(sound.duration() * per);
-      }
-    },
-
-    /**
-     * The step called within requestAnimationFrame to update the playback position.
-     */
-    step: function () {
-      var self = this;
-
-      // Get the Howl we want to manipulate.
-      var sound = self.playlist[self.index].howl;
-
-      // Determine our current seek position.
-      var seek = sound.seek() || 0;
-      timer.innerHTML = self.formatTime(Math.round(seek));
-      progress.style.width = (((seek / sound.duration()) * 100) || 0) + '%';
-
-      // If the sound is still playing, continue stepping.
-      if (sound.playing()) {
-        requestAnimationFrame(self.step.bind(self));
-      }
-    },
-
-    /**
-     * Toggle the playlist display on/off.
-     */
-    togglePlaylist: function () {
-      var self = this;
-      var display = (playlist.style.display === 'block') ? 'none' : 'block';
-
-      setTimeout(function () {
-        playlist.style.display = display;
-      }, (display === 'block') ? 0 : 500);
-      playlist.className = (display === 'block') ? 'pure-menu pure-menu-scrollable fadein' : 'pure-menu pure-menu-scrollable fadeout';
-    },
-
-    /**
-     * Toggle the volume display on/off.
-     */
-    toggleVolume: function () {
-      var self = this;
-      var display = (volume.style.display === 'block') ? 'none' : 'block';
-
-      setTimeout(function () {
-        volume.style.display = display;
-      }, (display === 'block') ? 0 : 500);
-      volume.className = (display === 'block') ? 'fadein' : 'fadeout';
-    },
-
-    /**
-     * Format the time from seconds to M:SS.
-     * @param  {Number} secs Seconds to format.
-     * @return {String}      Formatted time.
-     */
-    formatTime: function (secs) {
-      var minutes = Math.floor(secs / 60) || 0;
-      var seconds = (secs - minutes * 60) || 0;
-
-      return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+    // Stop the current track.
+    if (self.playlist[self.index].howl) {
+      self.playlist[self.index].howl.stop();
     }
+
+    // Reset progress.
+    progress.style.width = '0%';
+
+    // Play the new track.
+    self.play(index, true);
+  },
+
+  /**
+   * Set the volume and update the volume slider display.
+   * @param  {Number} val Volume between 0 and 1.
+   */
+  volume: function (val) {
+    var self = this;
+
+    // Update the global volume (affecting all Howls).
+    Howler.volume(val);
+
+    // Update the display on the slider.
+    var barWidth = (val * 90) / 100;
+    barFull.style.width = (barWidth * 100) + '%';
+    sliderBtn.style.left = (window.innerWidth * barWidth + window.innerWidth * 0.05 - 25) + 'px';
+  },
+
+  /**
+   * Seek to a new position in the currently playing track.
+   * @param  {Number} per Percentage through the song to skip.
+   */
+  seek: function (per) {
+    var self = this;
+
+    // Get the Howl we want to manipulate.
+    var sound = self.playlist[self.index].howl;
+
+    // Convert the percent into a seek position.
+    if (sound.playing()) {
+      sound.seek(sound.duration() * per);
+    }
+  },
+
+  /**
+   * The step called within requestAnimationFrame to update the playback position.
+   */
+  step: function () {
+    var self = this;
+
+    // Get the Howl we want to manipulate.
+    var sound = self.playlist[self.index].howl;
+
+    // Determine our current seek position.
+    var seek = sound.seek() || 0;
+    timer.innerHTML = self.formatTime(Math.round(seek));
+    progress.style.width = (((seek / sound.duration()) * 100) || 0) + '%';
+
+    // If the sound is still playing, continue stepping.
+    if (sound.playing()) {
+      requestAnimationFrame(self.step.bind(self));
+    }
+  },
+
+  /**
+   * Toggle the playlist display on/off.
+   */
+  togglePlaylist: function () {
+    var self = this;
+    var display = (playlist.style.display === 'block') ? 'none' : 'block';
+
+    setTimeout(function () {
+      playlist.style.display = display;
+    }, (display === 'block') ? 0 : 500);
+    playlist.className = (display === 'block') ? 'pure-menu pure-menu-scrollable fadein' : 'pure-menu pure-menu-scrollable fadeout';
+  },
+
+  /**
+   * Toggle the volume display on/off.
+   */
+  toggleVolume: function () {
+    var self = this;
+    var display = (volume.style.display === 'block') ? 'none' : 'block';
+
+    setTimeout(function () {
+      volume.style.display = display;
+    }, (display === 'block') ? 0 : 500);
+    volume.className = (display === 'block') ? 'fadein' : 'fadeout';
+  },
+
+  /**
+   * Format the time from seconds to M:SS.
+   * @param  {Number} secs Seconds to format.
+   * @return {String}      Formatted time.
+   */
+  formatTime: function (secs) {
+    var minutes = Math.floor(secs / 60) || 0;
+    var seconds = (secs - minutes * 60) || 0;
+
+    return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+  }
 };
 
 // Update the height of the wave animation.
