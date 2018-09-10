ac43d9416fa93897b39cb2074e319bed7833fe52
Jasonnor <wujason810@gmail.com>
2018-09-10 13:53:31 +0800

    Remove useless file

--- scripts/tools/combine.cmd
+++ scripts/tools/combine.cmd
@@ -4,3 +4,8 @@ type .\scripts\vudio.min.js >> .\scripts\scripts.min.js
 type .\scripts\utility.min.js >> .\scripts\scripts.min.js
 type .\scripts\player.min.js >> .\scripts\scripts.min.js
 type .\scripts\video.min.js >> .\scripts\scripts.min.js
+
+del .\scripts\vudio.min.js
+del .\scripts\utility.min.js
+del .\scripts\player.min.js
+del .\scripts\video.min.js
