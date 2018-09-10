c4f048185165e9ac59dc82a98468dfeefc6f3799
Jasonnor <wujason810@gmail.com>
2018-09-10 13:47:35 +0800

    Minifiy scripts & stylesheet

---
+++ scripts/tools/combine.cmd
@@ -0,0 +1,6 @@
+del .\scripts\scripts.min.js
+
+type .\scripts\vudio.min.js >> .\scripts\scripts.min.js
+type .\scripts\utility.min.js >> .\scripts\scripts.min.js
+type .\scripts\player.min.js >> .\scripts\scripts.min.js
+type .\scripts\video.min.js >> .\scripts\scripts.min.js
