c461033846ade6753732bbb1a3992b0f400a0789
Jasonnor <wujason810@gmail.com>
2022-05-15 21:21:28 +0800

    chore: update path to json scripts format

--- scripts/tools/_image_path_to_json.py
+++ scripts/tools/_image_path_to_json.py
@@ -1,16 +1,19 @@
 import os
 import errno
+import json
+import sys
+
 
 def path_hierarchy(path):
     path_save = '/' + path.split('\\', 4)[4].replace('\\', '/')
     hierarchy = {
         'path': path_save,
     }
-
     try:
         hierarchy['images'] = [
             path_hierarchy(os.path.join(path, contents))
-            for contents in os.listdir(path) if 'temp' not in contents and 'favicon' not in contents and 'title' not in contents
+            for contents in os.listdir(path) if
+            'temp' not in contents and 'favicon' not in contents and 'title' not in contents
         ]
         names = path.split('\\')
         hierarchy['name'] = names[len(names) - 1]
@@ -18,27 +21,23 @@ def path_hierarchy(path):
     except OSError as e:
         if e.errno != errno.ENOTDIR:
             raise
-
     return hierarchy
+
 
 def path_to_dict(path):
     d = {'name': os.path.basename(path)}
     if os.path.isdir(path):
         d['type'] = "directory"
-        d['children'] = [path_to_dict(os.path.join(path,x)) for x in os.listdir\
-(path)]
+        d['children'] = [path_to_dict(os.path.join(path, x)) for x in os.listdir(path)]
     else:
         d['type'] = "file"
     return d
 
-if __name__ == '__main__':
-    import json
-    import sys
 
+if __name__ == '__main__':
     try:
         directory = sys.argv[1]
     except IndexError:
         directory = "D:\\Download\\Touhou\\th-music-video-generator\\images"
 
-    print(json.dumps(path_hierarchy(directory), indent=2, sort_keys=True))
-    #print(json.dumps(path_to_dict(directory)))
+    print(json.dumps(path_hierarchy(directory), indent=2, sort_keys=True, ensure_ascii=False))
--- scripts/tools/_song_path_to_json.py
+++ scripts/tools/_song_path_to_json.py
@@ -1,5 +1,8 @@
 import os
 import errno
+import json
+import sys
+
 
 def path_hierarchy(path):
     path_save = '/' + path.split('\\', 4)[4].replace('\\', '/')
@@ -7,41 +10,36 @@ def path_hierarchy(path):
     hierarchy = {
         'name': names[len(names) - 1],
         'path': path_save,
-        'zcharacter': names[len(names) - 2],
-        'zkeyword': names[len(names) - 2],
+        'character': names[len(names) - 2],
+        'keyword': names[len(names) - 2],
     }
-
     try:
         hierarchy['songs'] = [
             path_hierarchy(os.path.join(path, contents))
             for contents in os.listdir(path)
         ]
-        del hierarchy['zcharacter']
-        del hierarchy['zkeyword']
+        del hierarchy['character']
+        del hierarchy['keyword']
     except OSError as e:
         if e.errno != errno.ENOTDIR:
             raise
-
     return hierarchy
+
 
 def path_to_dict(path):
     d = {'name': os.path.basename(path)}
     if os.path.isdir(path):
         d['type'] = "directory"
-        d['children'] = [path_to_dict(os.path.join(path,x)) for x in os.listdir\
-(path)]
+        d['children'] = [path_to_dict(os.path.join(path, x)) for x in os.listdir(path)]
     else:
         d['type'] = "file"
     return d
 
-if __name__ == '__main__':
-    import json
-    import sys
 
+if __name__ == '__main__':
     try:
         directory = sys.argv[1]
     except IndexError:
         directory = "D:\\Download\\Touhou\\th-music-video-generator\\audio\\temp"
 
-    print(json.dumps(path_hierarchy(directory), indent=2, sort_keys=True))
-    #print(json.dumps(path_to_dict(directory)))
+    print(json.dumps(path_hierarchy(directory), indent=2, sort_keys=True, ensure_ascii=False))
